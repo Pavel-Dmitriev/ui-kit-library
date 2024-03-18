263387f36f53e761dbbdee5924268c297a745c44
Pavel Dmitriev <pdmitriev@fil-it.ru>
2024-03-18 22:48:45 +0300

    feat: Формирование vite.config.ts под создание библиотеки компонентов.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "UiKitLibrary",
+      fileName: "ui-kit-library",
+    },
+    rollupOptions: {
+      external: ["react"],
+    },
+  },
   plugins: [react()],
-})
+});
